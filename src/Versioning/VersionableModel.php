26d695a9b151dbe9a6c9bfcbd83e2d1e073b118a
Aden Fraser <aden@jacobbailey.com>
2015-11-14 22:03:33 +0000

    Add Versioning Base

--- src/Versioning/VersionableModel.php
+++ src/Versioning/VersionableModel.php
@@ -13,4 +13,29 @@ trait VersionableModel
     {
         return $this->morphMany(\LaravelFlare\Versioning\Version::class, 'model');
     }
+
+    /**
+     * Revert to the previous version of the Model.
+     *
+     * This is the most recent version in the flare_versions 
+     * table for this model instance (based on model_id
+     * and model_type).
+     * 
+     * @return
+     */
+    public function revert()
+    {
+
+    }
+
+    /**
+     * Revert to a specified version of the Model.
+     *
+     * @return
+     */
+    public function revertTo($hash)
+    {
+
+    }
+
 }
